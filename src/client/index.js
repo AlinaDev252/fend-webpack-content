bb6a52e14c23642ef22df711441f80e8ec23b8cc
AlinaDev252 <costachealina25@gmail.com>
2021-03-02 14:28:21 +0200

    Add file loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+import { img } from "./file.png";
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
