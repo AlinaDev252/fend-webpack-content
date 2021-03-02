3dc026812d5f3c3e90866e27f6494bfbad1bcc7b
AlinaDev252 <costachealina25@gmail.com>
2021-03-02 14:54:36 +0200

    Add html webpack plugin

---
+++ src/client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/client/index.js",
+};
+
+module: {
+	rules: [
+		{
+			test: "/.js$/",
+			exclude: /node_modules/,
+			loader: "babel-loader",
+			loader: "file-loader",
+		},
+	];
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		}),
+	];
+}
